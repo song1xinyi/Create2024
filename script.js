0e0cdd1fdea2ed3d1a103b51dfe67c6ef24b4304
song1xinyi <161468747+song1xinyi@users.noreply.github.com>
2024-03-12 23:17:14 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+document.write("hello world!");
+function sayHello(){
+  var response=prompt("What is your name?");
+  alert("hello "+response+",have a nice day!");
+};
+//sayHello();
+
+function showTopic(){
+  var x = document.getElementById('demo');
+  x.style.fontSize = "25px";
+  x.style.color = "red";
+}
+showTopic();
